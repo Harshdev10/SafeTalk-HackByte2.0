506aca1e313c348e141ba70130badc3d9546b661
Harshdev10 <harshdevtripathi1234@gmail.com>
2024-04-06 02:31:13 +0530

    Landing page done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import GlobalStyles from './globalStyles'
+import { Navbar, Footer } from './components';
+import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <BrowserRouter>
+          <GlobalStyles />
+          <ScrollToTop />
+          <Navbar />
+          <Home/>
+          <Routes>
+            <Route path='/' exact component={Home} />
+            <Route path='/services' component={Services} />
+            <Route path='/products' component={Products} />
+            <Route path='/sign-up' component={SignUp} />
+            </Routes>
+          <Footer />
+      </BrowserRouter>
+        
+    
   );
 }
 
